e309335abfed091c7dfa961c200c859915760c27
Calin Cristescu <calin.cristescu@jobleads.de>
2019-09-29 11:30:34 +0200

    Update unit tests

--- myservice/tests/test_hello.py
+++ myservice/tests/test_hello.py
@@ -1,7 +1,18 @@
+from myservice.app import app as tested_app
 import unittest
+import json
 
 
 class TestSomething(unittest.TestCase):
 
-    def test_my_view(self):
-        pass
+    def test_home_view(self):
+        # creating a FlaskClient instance to interact with the app
+        tested_app.testing = True
+        client = tested_app.test_client()
+
+        # calling /hello endpoint
+        hello = client.get('/hello')
+
+        # asserting the body
+        body = json.loads(str(hello.data, 'utf8'))
+        self.assertEqual(body['Hello'], 'World')
--- myservice/tests/test_home.py
+++ myservice/tests/test_home.py
@@ -1,7 +1,18 @@
+from myservice.app import app as tested_app
 import unittest
+import json
 
 
 class TestSomething(unittest.TestCase):
 
-    def test_my_view(self):
-        pass
+    def test_home_view(self):
+        # creating a FlaskClient instance to interact with the app
+        tested_app.testing = True
+        client = tested_app.test_client()
+
+        # calling / endpoint
+        home = client.get('/')
+
+        # asserting the body
+        body = json.loads(str(home.data, 'utf8'))
+        self.assertEqual(len(body), 0)
